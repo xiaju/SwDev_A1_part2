2d86374a9605999676682b8c17fd1efa04059035
moustafaabdel <46932665+moustafaabdel@users.noreply.github.com>
2020-01-29 12:50:45 -0500

    update array.h to have four different array types

--- part2/array.h
+++ part2/array.h
@@ -2,26 +2,102 @@
 //lang::CwC
 #include "object.h"
 
-// Represents an array that stores elements of a similar, generic type <T> at the array's indexes.
-template <class T>
-class Array : public Object { 
+// Represents an array that stores elements of type int
+class IntArray : public Object { 
 	public:
 
-		Array(); //default constructor
-		Array(size_t size, T* vals); //convenience constructor, initializes new Array with all elements given
-		~Array(); //destructor
+		IntArray(); //default constructor
+		IntArray(size_t size, int* vals); //convenience constructor, initializes new Array with all elements given
+		
+		~IntArray(); //destructor
 
-		void push_back(T e); // Appends e to end
-		void add(size_t i, T e); // Inserts e at i
-		void add_all(size_t i, Array* c); // Inserts all of elements in c into this list at i
+		void push_back(int e); // Appends e to end
+		void add(size_t i, int e); // Inserts e at i
+		void add_all(size_t i, IntArray* c); // Inserts all of elements in c into this list at i
 		void clear(); // Removes all of elements from this list
-		bool equals(Object* o); // Compares o with this list for equality.
-		T get(size_t index); // Returns the element at index
-		size_t hash(); // Returns the hash code value for this list.
-		size_t index_of(Object* o); // Returns the index of the first occurrence of o, or >size() if not there
-		T remove(size_t i); //Removes the element at i, returns the removed element 
-		T set(size_t i, T e); // Replaces the element at i with e
+		virtual bool equals(Object* o); // Compares o with this list for equality.
+		int get(size_t index); // Returns the element at index
+		virtual size_t hash(); // Returns the hash code value for this list.
+		size_t index_of(int o); // Returns the index of the first occurrence of o, or >size() if not there
+		int remove(size_t i); //Removes the element at i, returns the removed element 
+		int set(size_t i, int e); // Replaces the element at i with e
 		size_t size(); // Return the number of elements in the collection
 		bool is_empty(); // Is this array empty (0 elements stored)?
 		void print(); // Print this object including the elements it stores
 };
+
+// Represents an array that stores elements of type bool
+class BoolArray : public Object { 
+	public:
+
+		BoolArray(); //default constructor
+		BoolArray(size_t size, int* vals); //convenience constructor, initializes new Array with all elements given
+		
+		~BoolArray(); //destructor
+
+		void push_back(int e); // Appends e to end
+		void add(size_t i, int e); // Inserts e at i
+		void add_all(size_t i, BoolArray* c); // Inserts all of elements in c into this list at i
+		void clear(); // Removes all of elements from this list
+		virtual bool equals(Object* o); // Compares o with this list for equality.
+		int get(size_t index); // Returns the element at index
+		virtual size_t hash(); // Returns the hash code value for this list.
+		size_t index_of(int o); // Returns the index of the first occurrence of o, or >size() if not there
+		int remove(size_t i); //Removes the element at i, returns the removed element 
+		int set(size_t i, int e); // Replaces the element at i with e
+		size_t size(); // Return the number of elements in the collection
+		bool is_empty(); // Is this array empty (0 elements stored)?
+		void print(); // Print this object including the elements it stores
+};
+
+// Represents an array that stores elements of type float
+class FloatArray : public Object { 
+	public:
+
+		FloatArray(); //default constructor
+		FloatArray(size_t size, float* vals); //convenience constructor, initializes new Array with all elements given
+		
+		~FloatArray(); //destructor
+
+		void push_back(float e); // Appends e to end
+		void add(size_t i, float e); // Inserts e at i
+		void add_all(size_t i, FloatArray* c); // Inserts all of elements in c into this list at i
+		void clear(); // Removes all of elements from this list
+		virtual bool equals(Object* o); // Compares o with this list for equality.
+		float get(size_t index); // Returns the element at index
+		virtual size_t hash(); // Returns the hash code value for this list.
+		size_t index_of(float o); // Returns the index of the first occurrence of o, or >size() if not there
+		float remove(size_t i); //Removes the element at i, returns the removed element 
+		float set(size_t i, float e); // Replaces the element at i with e
+		size_t size(); // Return the number of elements in the collection
+		bool is_empty(); // Is this array empty (0 elements stored)?
+		void print(); // Print this object including the elements it stores
+};
+
+// Represents an array that stores elements of type string
+class StringArray : public Object { 
+	public:
+
+		StringArray(); //default constructor
+		StringArray(size_t size, char** vals); //convenience constructor, initializes new Array with all elements given
+		
+		~StringArray(); //destructor
+
+		void push_back(char* e); // Appends e to end
+		void add(size_t i, char* e); // Inserts e at i
+		void add_all(size_t i, StringArray* c); // Inserts all of elements in c into this list at i
+		void clear(); // Removes all of elements from this list
+		virtual bool equals(Object* o); // Compares o with this list for equality.
+		char* get(size_t index); // Returns the element at index
+		virtual size_t hash(); // Returns the hash code value for this list.
+		size_t index_of(char* o); // Returns the index of the first occurrence of o, or >size() if not there
+		char* remove(size_t i); //Removes the element at i, returns the removed element 
+		char* set(size_t i, char* e); // Replaces the element at i with e
+		size_t size(); // Return the number of elements in the collection
+		bool is_empty(); // Is this array empty (0 elements stored)?
+		void print(); // Print this object including the elements it stores
+};
+
+
+
+
